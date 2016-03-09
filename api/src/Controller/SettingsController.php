116e757bc735048a94bfab1af9cfbe2cee5f280a
Viktor Ban <viktor.ban@gmail.com>
2016-03-09 19:35:13 +0100

    Implemented SettingsController.

--- api/src/Controller/SettingsController.php
+++ api/src/Controller/SettingsController.php
@@ -15,4 +15,37 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class SettingsController extends Controller {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function getAll(Request $request, Application $app) {
+    $res = $this->db()->fetchAll("SELECT * FROM settings");
+    $settings = [];
+    foreach ($res as $row) {
+      $settings[$row['key']] = $row['value'];
+    }
+    return $app->json($settings);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function get(Request $request, Application $app) {
+    $id = $request->get('id');
+
+    $setting = $this->db()->fetchAssoc("SELECT * FROM settings WHERE id = ?", [$id]);
+    return $app->json($setting['value']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function modify(Request $request, Application $app) {
+    $id = $request->get('id');
+    $value = $request->getContent();
+
+    $res = $this->db()->update('settings', ['value' => $value], ['id' => $id]);
+    return $app->json($res > 0);
+  }
+
 }
