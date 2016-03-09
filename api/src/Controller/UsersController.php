b181cabbfa8473c1f6a05846b6d9e2504c08da18
Viktor Ban <viktor.ban@gmail.com>
2016-03-09 23:27:41 +0100

    Implemented /self.

--- api/src/Controller/UsersController.php
+++ api/src/Controller/UsersController.php
@@ -22,9 +22,28 @@ class UsersController extends Controller {
    */
   public static function routes() {
     $routes = parent::routes();
-    $routes['get']['/logout'] = 'logout';
     $routes['post']['/login'] = 'login';
+    $routes['get']['/logout'] = 'logout';
+    $routes['get']['/self'] = 'self';
     return $routes;
+  }
+
+  /**
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   * @param \Silex\Application $app
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   */
+  public function login(Request $request, Application $app) {
+    $user = $request->get('user');
+    $pass = $request->get('pass');
+
+    $obj = User::loadByCredentials($user, $pass);
+    if ($obj) {
+      $_SESSION['uid'] = $obj->getId();
+      return $app->json($obj->get());
+    }
+    return $app->json(FALSE);
   }
 
   /**
@@ -44,16 +63,8 @@ class UsersController extends Controller {
    *
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
-  public function login(Request $request, Application $app) {
-    $user = $request->get('user');
-    $pass = $request->get('pass');
-
-    $obj = User::loadByCredentials($user, $pass);
-    if ($obj) {
-      $_SESSION['uid'] = $obj->getId();
-      return $app->json($obj->get());
-    }
-    return $app->json(FALSE);
+  public function self(Request $request, Application $app) {
+    return $app->json(User::currentUser()->get());
   }
 
 }
