d0b18be86e3c51e99d065cdbb18c913a8c2214b3
Viktor Ban <viktor.ban@gmail.com>
2016-03-12 10:24:53 +0100

    /sensor Implemented.

--- api/src/Controller/SensorController.php
+++ api/src/Controller/SensorController.php
@@ -24,4 +24,10 @@ class SensorController extends Controller {
     return $routes;
   }
 
+  public function getAll(Request $request, Application $app) {
+    // Random temperature between 17.0 and 22.0 degrees celsius.
+    $temp = rand(2902, 2952);
+    return $app->json($temp / 10);
+  }
+
 }
