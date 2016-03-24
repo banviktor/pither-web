bd938967704debff8390ba9abbddba951525b574
Viktor Ban <viktor.ban@gmail.com>
2016-03-24 08:16:24 +0100

    User list has checkboxes, dual password inputs are nicer.

--- app/user/userListController.js
+++ app/user/userListController.js
@@ -1,15 +1,40 @@
 angular.module('PiTher').controller('UserListController', ['$scope', '$routeParams', '$http', '$location', function ($scope, $routeParams, $http) {
   var controller = this;
   this.users = [];
+  this.selected = {};
+  this.allSelected = false;
   this.action = 'list';
   this.newUser = {
     unit: 'c'
   };
 
+  this.selectAll = function() {
+    controller.selected = {};
+    for (var i = 0; i < controller.users.length; ++i) {
+      var id = controller.users[i].id;
+      controller.selected[id] = controller.allSelected;
+    }
+  };
   this.refreshList = function () {
     $http.get('api/users').then(
       function successCallback(response) {
-        controller.users = response.data.data;
+        $scope.clearNotifications();
+        if (response.data.success) {
+          controller.users = response.data.data;
+          for (var i = 0; i < controller.users.length; ++i) {
+            var id = controller.users[i].id;
+            if (!controller.selected.hasOwnProperty(id)) {
+              controller.selected[id] = false;
+            }
+          }
+        }
+        else {
+          $scope.notifications.danger = response.data.errors;
+        }
+      },
+      function errorCallback(response) {
+        $scope.clearNotifications();
+        $scope.notifications.danger = response.data.errors;
       }
     );
   };
