227e69cc599c9ad8578203a9177d964e62862ddf
Viktor Ban <viktor.ban@gmail.com>
2016-04-11 11:26:37 +0200

    Kelvin to ... filter.

--- app/filters.js
+++ app/filters.js
@@ -1,5 +1,28 @@
-angular.module('PiTher').filter('ucfirst', function () {
-  return function (input) {
-    return input.charAt(0).toUpperCase() + input.substr(1);
-  }
-});
+angular.module('PiTher')
+  .filter('ucfirst', function () {
+    return function (input) {
+      return input.charAt(0).toUpperCase() + input.substr(1);
+    }
+  })
+  .filter('kelvin2', function () {
+    return function(input, params) {
+      var userUnit = params || 'c';
+
+      kelvin = 0;
+      switch (userUnit.toLowerCase()) {
+        case 'k':
+          kelvin = input;
+          break;
+
+        case 'c':
+          kelvin = input - 273.15;
+          break;
+
+        case 'f':
+          kelvin = input * 9 / 5 - 459.67;
+          break;
+      }
+      return Math.round(kelvin * 10) / 10;
+    }
+  })
+;
